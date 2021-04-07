4723da04cc63427814b6df63bae38e79aa7d1dc3
jure <www.mephit.it@gmail.com>
2021-04-08 01:02:19 +0200

    - language selection - new platform: browser - new language: Spanish

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,14 @@
 var _s = s;
 var app_started = new Date();
 
+function filePath(path) {
+    return ( device.platform === "Android" ? "/android_asset/www" : "" ) + path;
+}
+
+function setPseudoElContent(selector, value) {    
+    document.styleSheets[0].addRule(selector, 'content: "' + value + '";');
+}
+
 function makeTabs(tabs) {
     tabs.forEach(tab => {
         const [ button, content, callback ] = tab;
@@ -21,9 +29,203 @@ function makeTabs(tabs) {
     firstButton.dispatchEvent(new Event('click'));
 }
 
+function makeSelector(options) {
+
+    options = _.defaults(options, {
+        element: null,
+        options: [],
+        value: null,
+        onChange: '',
+        trigger: true,
+    });
+
+    const selectorElement  = options.element;
+    const selectorOptions  = options.options;
+    const selectedValue    = (_.isUndefined(options.value) || _.isNull(options.value)) ? selectorElement.value : options.value;
+    const onChangeFuncPath = options.onChange;
+    const triggerChange    = options.trigger;
+    
+    selectorElement.classList.add('hidden-selector');
+    selectorElement.setAttribute('data-onchange', onChangeFuncPath);
+
+    const linkElement = document.createElement('span');
+    linkElement.classList.add('hidden-selector-link');
+    selectorElement.parentNode.appendChild(linkElement);
+
+    const styleElement = document.createElement('style');
+    styleElement.id = selectorElement.id + '-style';
+    document.querySelector('head').appendChild(styleElement);
+
+    if (device.platform !== 'browser') {
+
+        linkElement.addEventListener('click', (ev) => {
+            var e = document.createEvent('MouseEvents');
+            e.initMouseEvent('mousedown', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            selectorElement.dispatchEvent(e);
+        });
+    
+        selectorElement.addEventListener('change', () => {
+            linkElement.textContent = selectorElement.options[selectorElement.selectedIndex].text;
+        });
+    
+    } else {
+
+        linkElement.addEventListener('click', (ev) => {
+            selectorElement.setAttribute('size', selectorElement.options.length);
+            selectorElement.classList.remove('hidden-selector');
+            selectorElement.classList.add('browser-selector');
+            selectorOverlay(selectorElement, true);
+        });
+
+        selectorElement.addEventListener('change', () => {
+            selectorElement.classList.remove('browser-selector');
+            selectorElement.classList.add('hidden-selector');
+            linkElement.textContent = selectorElement.options[selectorElement.selectedIndex].text;
+
+            selectorOverlay(selectorElement, false);
+
+            document.getElementById(`${selectorElement.id}-style`).innerHTML = `
+                #${selectorElement.id} option:nth-child(${selectorElement.selectedIndex + 1})::before {
+                    content: "${selectorElement.options[selectorElement.selectedIndex].text}";
+                }
+            `;
+
+        });
+    
+    }
+
+    populateSelector(options);
+
+}
+
+function populateSelector(options) {
+
+    options = _.defaults(options, {
+        element: null,
+        options: [],
+        value: null,
+        onChange: '',
+        trigger: true,
+    });
+
+    if (!_.has(options.element.dataset, 'onchange')) throw new Error("Elemento non inizializzato");
+
+    const selectorElement  = options.element;
+    const selectorOptions  = options.options;
+    const selectedValue    = (_.isUndefined(options.value) || _.isNull(options.value)) ? selectorElement.value : options.value;
+    const onChangeFuncPath = options.onChange;
+    const triggerChange    = options.trigger;
+
+    // individua la funzione onchange
+    let funcPath = onChangeFuncPath || selectorElement.getAttribute('data-onchange');
+    funcPath = funcPath.split('.');
+    const that = _.get(window, funcPath[0]);
+    const func = _.get(window, funcPath).bind(that);
+    
+    // rimuovi il listener onchange
+    selectorElement.removeEventListener('change', func);
+
+    // svuota le options
+    let i = selectorElement.length;
+    while (i--) selectorElement.remove(i);
+
+    // aggiungi le options
+    selectorOptions.sort((a, b) => a[0].localeCompare(b[0], this.lang));
+    selectorOptions.forEach(so => {
+        const [ text, value ] = so;
+        selectorElement.add(new Option(text, value));
+    });
+
+    // setta la option
+    selectorElement.value = selectedValue;
+
+    // se la option non esiste più, setta la prima
+    if (selectorElement.selectedIndex === -1) selectorElement.selectedIndex = 0;
+
+    // setta il testo del link
+    const linkElement = selectorElement.parentNode.querySelector('span');
+    linkElement.textContent = selectorElement.options[selectorElement.selectedIndex].text;
+
+    selectorElement.addEventListener('change', func);
+
+    if (triggerChange) selectorElement.dispatchEvent(new Event('change'));
+
+}
+
+function renameSelectorOptions(options) {
+
+    options = _.defaults(options, {
+        element: null,
+        options: [],
+    });
+
+    const selectorElement  = options.element;
+    const selectorOptions  = options.options;
+
+    selectorOptions.sort((a, b) => a[0].localeCompare(b[0], app.lang));
+
+    _.each(selectorElement.options, opt => {
+        const match = _.find(selectorOptions, pair => pair[1] === opt.value);
+        if (match) opt.text = match[0];
+    });
+    
+    const $selectorElement = $(selectorElement);
+    
+    selectorOptions.forEach(pair => {
+        const [ text, value ] = pair;
+        const optionElement = Array.from(selectorElement.options).find(optEl => optEl.value === value);
+        $selectorElement.append(optionElement);
+    });
+
+    if (!_.has(selectorElement.dataset, 'onchange')) return;
+
+    const linkElement = selectorElement.parentNode.querySelector('span');
+    if (selectorElement.selectedIndex !== -1) {
+        linkElement.textContent = selectorElement.options[selectorElement.selectedIndex].text;
+    } else {
+        linkElement.textContent = '';
+    }
+}
+
+function selectorOverlay(selectorElement, show) {
+
+    const parentTab = $(selectorElement).closest('.app-ui-tab').get(0);
+    let overlayElement = parentTab.querySelector('.browser-selector-overlay');
+
+    if (!overlayElement) {
+
+        overlayElement = document.createElement('div');
+        overlayElement.classList.add('browser-selector-overlay');
+        overlayElement.style.display = 'none';
+        parentTab.insertBefore(overlayElement, parentTab.children[0]);
+
+        overlayElement.addEventListener('click', ($ev) => {
+            selectorElement.dispatchEvent(new Event('change'));
+        });
+
+    }
+
+    let tabBar = document.getElementById('tab-bar');
+    
+    if (show) {
+        overlayElement.style.display = 'block';
+        tabBar.style.pointerEvents = 'none';
+        tabBar.style.opacity = 0.7;
+    } else {
+        overlayElement.style.display = 'none' ;
+        tabBar.style.pointerEvents = 'auto';
+        tabBar.style.opacity = 1;
+    }
+
+}
+
 function l(str) {
-    console.log("str",str,app.currentLanguage,app.currentLanguage.getString(str))
-    return app.currentLanguage.getString(str);
+    const len = arguments.length;
+    str = app.currentLanguage.getString(str);
+    for (let i = len - 1; i > 0; i--) {
+        str = str.split('%' + i).join(arguments[i]);
+    }
+    return str;
 }
 
 var app = {
@@ -49,6 +251,11 @@ var app = {
 
     },
 
+    setLang: function(langId) {
+        this.lang = langId;
+        this.currentLanguage = _.find(languages, language => language.getId() === langId);
+    },
+
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
@@ -56,18 +263,14 @@ var app = {
     onDeviceReady: function() {
 
         const defaultLang = _.find(languages, language => language.isDefault());
-        this.currentLanguage = defaultLang;
-        this.lang = defaultLang.getId();
         navigator.globalization.getLocaleName((locale) => {
             const currentLang = locale.value.split(',')[0].split('-')[0];
             const supportedLang = _.find(languages, language => language.getId() === currentLang);
-            if (supportedLang) {
-                this.currentLanguage = supportedLang;
-                this.lang = supportedLang.getId();
-            }
+            this.setLang(supportedLang ? supportedLang.getId() : defaultLang.getId());
             this.prepareApp();
         }, (e) => {
             console.error(e);
+            this.setLang(defaultLang.getId());
             this.prepareApp();
         });
 
@@ -77,7 +280,7 @@ var app = {
 
         this.appElement               = document.getElementById('app');
         
-        this.snapshotElement          = document.getElementById('snapshot');
+        // this.snapshotElement          = document.getElementById('snapshot');
         this.ocrBlockElement          = document.getElementById('card-ocr-block');
         this.monstersContentElement   = document.getElementById('app-ui');
         this.optionsContentElement    = document.getElementById('app-options');
@@ -85,15 +288,14 @@ var app = {
         this.ocrTextElement           = document.getElementById('card-name');
         this.monsterListELement       = document.getElementById('monster-list');
         this.scenarioSelector         = document.getElementById('scenario-selector');
-        this.scenarioNameElement      = document.getElementById('scenario-name');
+        // this.scenarioNameElement      = document.getElementById('scenario-name');
 
         this.ocrTextElement.innerHTML = l('DEFAULT_OCR_TEXT');
 
+        // const filepathBG              = filePath("/sounds/bg.wav");
+        const filepathOK              = filePath("/sounds/match-yes.mp3");
+        const filepathKO              = filePath("/sounds/match-no.mp3");
         // mdls command in terminal to get file duration in seconds
-        const androidBasePath         = "/android_asset/www";
-        const filepathOK              = [androidBasePath, "sounds", "match-yes.mp3"].join('/');
-        const filepathKO              = [androidBasePath, "sounds", "match-no.mp3"].join('/');
-        // const filepathBG              = [androidBasePath, "sounds", "bg.wav"].join('/');
         this.sounds.ok                = {media: new Media(filepathOK), duration: 3736};
         this.sounds.ko                = {media: new Media(filepathKO), duration: 3736};
         // this.sounds.bg                = {media: new Media(filepathBG), duration: };
@@ -103,28 +305,16 @@ var app = {
         monsters = monsters.map(item => new Monster(item));
         
         scenarios = scenarios.map(item => new Scenario(item));
-        scenarios.sort((a, b) => a.getName().localeCompare(b.getName(), this.lang));
 
         options.initialize();
-
-        this.applyLanguage();
 
         this.onAppReady();
     },
 
-    applyLanguage: function() {
-
-        // document.querySelector('html').className = this.lang;
-
-        document.querySelectorAll('[data-lang]').forEach(element => {
-            const str = element.getAttribute('data-lang');
-            element.innerHTML = l(str);
-        });
-
-    },
-
     // Update DOM on a Received Event
     onAppReady: function() {
+
+        this.appElement.style.visibility = 'visible';
         // this.sound('bg', 12000);
         
         // tab bar
@@ -141,18 +331,35 @@ var app = {
             ],
         ]);
 
-        this.populateScenarioSelector(options.readOption('scenarioId'));
-
-        this.scenarioNameElement.addEventListener('click', (ev) => {
-            var e = document.createEvent('MouseEvents');
-            e.initMouseEvent('mousedown', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-            this.scenarioSelector.dispatchEvent(e);
+        makeSelector({
+            element: this.scenarioSelector,
+            options: this.getAvailableScenarios().map(scenario => [scenario.getName(), scenario.getId()]),
+            value: options.readOption('scenarioId'),
+            onChange: 'app.selectScenario',
         });
+
+        this.applyLanguage();
 
         (function hideSplashScreen() {
             if (new Date() - app_started < 2000 ) setTimeout(hideSplashScreen, 20);
             else navigator.splashscreen.hide();
         })();
+
+    },
+
+    applyLanguage: function() {
+
+        // document.querySelector('html').className = this.lang;
+
+        document.querySelectorAll('[data-lang]').forEach(element => {
+            const str = element.getAttribute('data-lang');
+            element.innerHTML = l(str);
+        });
+
+        renameSelectorOptions({
+            element: this.scenarioSelector,
+            options: this.getAvailableScenarios().map(scenario => [scenario.getName(), scenario.getId()]),
+        });
 
     },
 
@@ -164,27 +371,6 @@ var app = {
 
     onResume: function() {
         if (options.readOption('ocr')) this.startCamera();
-    },
-
-    populateScenarioSelector: function(scenarioId) {
-        const scenarioSelector = this.scenarioSelector;
-
-        scenarioId = scenarioId || scenarioSelector.value;
-        scenarioSelector.removeEventListener('change', this.selectScenario);
-
-        let i = scenarioSelector.options.length;
-        while (i--) scenarioSelector.options[i] = null;
-        scenarios.forEach(scenario => {
-            if (options.isExpansionAvailable(scenario)) {
-                var option = new Option(`${scenario.getName()}`, scenario.getId());
-                scenarioSelector.add(option);
-            }
-        });
-        if (scenarioId) scenarioSelector.value = scenarioId;
-        if (scenarioSelector.options.selectedIndex === -1) scenarioSelector.options.selectedIndex = 0;
-
-        scenarioSelector.addEventListener('change', this.selectScenario.bind(this));
-        scenarioSelector.dispatchEvent(new Event('change'));
     },
 
     onTabMonstersActive: function() {
@@ -237,6 +423,10 @@ var app = {
         return scenarios.find(scenario => {
             return scenario.getId() === id;
         });
+    },
+
+    getAvailableScenarios: function() {
+        return _.filter(scenarios, scenario => options.isExpansionAvailable(scenario));
     },
 
     startCamera: function() {
@@ -310,7 +500,7 @@ var app = {
 
         const ocrTextElement  = this.ocrTextElement;
         // const ocrBlockElement = this.ocrBlockElement;
-        const snapshotElement = this.snapshotElement;
+        // const snapshotElement = this.snapshotElement;
         // const style           = ocrBlockElement.style;
 
         if (!data.foundText) {
@@ -405,13 +595,13 @@ var app = {
     },
 
     renderScenario: function() {
-        let scenarioName = '';
+        // let scenarioName = '';
         let monsterList = '';
         if (this.scenario) {
-            scenarioName = this.scenario.getName();
+            // scenarioName = this.scenario.getName();
             monsterList = options.readOption('group') ? this.scenario.getGroupedHTML() : this.scenario.getFlatHTML();
         }
-        this.scenarioNameElement.innerHTML = scenarioName;
+        // this.scenarioNameElement.innerHTML = scenarioName;
         this.monsterListELement.innerHTML = monsterList;
         this.applyCountOption();
     },
