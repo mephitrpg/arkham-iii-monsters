efe0eb9cd36871660d2fbd70e2241925b7a196ed
jure <www.mephit.it@gmail.com>
2021-04-08 11:01:10 +0200

    remove ignored files

--- platforms/android/app/src/main/assets/www/plugins/cordova-plugin-mobile-ocr/www/rectext.js
+++
@@ -1,9 +0,0 @@
-cordova.define("cordova-plugin-mobile-ocr.rectext", function(require, exports, module) {
-/*global cordova, module*/
-
-module.exports = {
-    recText: function (sourceType, imageSource, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Textocr", "recText", [sourceType, imageSource]);
-    }
-};
-});
--- platforms/android/platform_www/js/rectext.js
+++
@@ -1,7 +0,0 @@
-/*global cordova, module*/
-
-module.exports = {
-    recText: function (sourceType, imageSource, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Textocr", "recText", [sourceType, imageSource]);
-    }
-};
